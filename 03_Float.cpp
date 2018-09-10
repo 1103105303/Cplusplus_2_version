69280be59f7b77ff25c7d5319c9f0a5e23ae1f3c
Niklas <38382776+1103105303@users.noreply.github.com>
2018-09-10 22:07:42 +0800

    Update 03_Float.cpp

--- 03_Float.cpp
+++ 03_Float.cpp
@@ -6,8 +6,8 @@ float FLOAT(float input1, float input2)
 	temp = (int)(input1 / input2 / 0.1);
 	temp2 =(int)(input1 / input2);
 	temp2 *= 10;
-	temp2 = temp - temp2; // 取最後一位
-	temp = (temp + temp2) * 0.1; // 四捨五入
+	temp2 = temp - temp2; //get last number
+	temp = (temp + temp2) * 0.1; //to the nearest whole number
 	return (float)(temp * input2);
 }
 
